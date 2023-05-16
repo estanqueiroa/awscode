247b000f8e24ae3727d2197b7b1cee89e9b55a63
estanqueiroa <artur@estanqueiro.com.br>
2023-05-16 17:09:44 -0300

    v1.0

--- python/movemouse/movemouse.py
+++ python/movemouse/movemouse.py
@@ -12,17 +12,16 @@ try:
         #calculate height and width of screen
         w, h = list(pyautogui.size())[0], list(pyautogui.size())[1]
 
-        while True:
-            time.sleep(10)
-            #move mouse at random location in screen, change it to your preference
-            # pyautogui.moveTo(rnd.randrange(0, w), 
-            #                 rnd.randrange(0, h))#, duration = 0.1)
-            
-            pyautogui.move(0, 50)       # move the mouse down 50 pixels.
-            pyautogui.move(-30, 0)      # move the mouse left 30 pixels.
-            pyautogui.move(0, -50)      # move the mouse up 50 pixels.
-            pyautogui.move(30, 0)      # move the mouse right 30 pixels.
-            pyautogui.click(clicks=1)  # single-click the left mouse button
+        time.sleep(10)
+        #move mouse at random location in screen, change it to your preference
+        # pyautogui.moveTo(rnd.randrange(0, w), 
+        #                 rnd.randrange(0, h))#, duration = 0.1)
+        
+        pyautogui.move(0, 50)       # move the mouse down 50 pixels.
+        pyautogui.move(-30, 0)      # move the mouse left 30 pixels.
+        pyautogui.move(0, -50)      # move the mouse up 50 pixels.
+        pyautogui.move(30, 0)      # move the mouse right 30 pixels.
+        pyautogui.click(clicks=1)  # single-click the left mouse button
 
 except KeyboardInterrupt:
     print('\n')
