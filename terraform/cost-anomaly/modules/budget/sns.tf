ebcae4433f92c2b02c1fa842bff1ee3583253fa0
estanqueiroa <50240388+estanqueiroa@users.noreply.github.com>
2025-10-30 12:12:22 -0300

    Update sns.tf

--- terraform/cost-anomaly/modules/budget/sns.tf
+++ terraform/cost-anomaly/modules/budget/sns.tf
@@ -1,10 +1,4 @@
-####################################################################################################
-# © 2025 Amazon Web Services, Inc. or its affiliates. All Rights Reserved.                         #
-#                                                                                                  #
-# This AWS Content is provided subject to the terms of the AWS Customer Agreement                  #
-# available at http://aws.amazon.com/agreement or other written agreement between                  #
-# Customer and either Amazon Web Services, Inc. or Amazon Web Services EMEA SARL or both.          #
-####################################################################################################
+
 
 resource "aws_sns_topic" "cost_anomaly_updates" {
 
@@ -99,4 +93,5 @@ resource "aws_sns_topic_policy" "default" {
 
   policy = data.aws_iam_policy_document.sns_topic_policy[0].json
 }
+
 
