e2e1d92cf586670f9cdc6312b85492eeec1c0daa
estanqueiroa <50240388+estanqueiroa@users.noreply.github.com>
2025-10-30 12:11:58 -0300

    Update locals.tf

--- terraform/cost-anomaly/modules/budget/locals.tf
+++ terraform/cost-anomaly/modules/budget/locals.tf
@@ -1,10 +1,4 @@
-####################################################################################################
-# © 2025 Amazon Web Services, Inc. or its affiliates. All Rights Reserved.                         #
-#                                                                                                  #
-# This AWS Content is provided subject to the terms of the AWS Customer Agreement                  #
-# available at http://aws.amazon.com/agreement or other written agreement between                  #
-# Customer and either Amazon Web Services, Inc. or Amazon Web Services EMEA SARL or both.          #
-####################################################################################################
+
 
 locals {
   sns_topic_arn = var.subscriber_type == "SNS" ? aws_sns_topic.cost_anomaly_updates[0].arn : null
