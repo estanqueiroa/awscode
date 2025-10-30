5fe74a02b34a08d7ed6982cb139d1702ec0e61d8
estanqueiroa <50240388+estanqueiroa@users.noreply.github.com>
2025-10-30 12:12:29 -0300

    Update variables.tf

--- terraform/cost-anomaly/modules/budget/variables.tf
+++ terraform/cost-anomaly/modules/budget/variables.tf
@@ -1,10 +1,3 @@
-####################################################################################################
-# © 2025 Amazon Web Services, Inc. or its affiliates. All Rights Reserved.                         #
-#                                                                                                  #
-# This AWS Content is provided subject to the terms of the AWS Customer Agreement                  #
-# available at http://aws.amazon.com/agreement or other written agreement between                  #
-# Customer and either Amazon Web Services, Inc. or Amazon Web Services EMEA SARL or both.          #
-####################################################################################################
 
 variable "budget_name" {
   description = "Name of the budget"
@@ -75,3 +68,4 @@ variable "anomaly_absolute" {
 
 
 }
+
